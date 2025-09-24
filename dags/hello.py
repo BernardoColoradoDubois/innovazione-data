39a72f4879305471014f466ee3bc964506d857c6
berni-rocket <bernardo.colorado@therocketcode.com>
2025-09-23 22:53:02 -0600

    ignorando logs

--- dags/hello.py
+++ dags/hello.py
@@ -1,18 +1,18 @@
 from datetime import datetime
 
-from airflow.sdk import DAG, task
+from airflow.sdk import DAG
 from airflow.providers.standard.operators.bash import BashOperator
 from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.standard.operators.python import BranchPythonOperator
 from airflow.providers.standard.operators.empty import EmptyOperator
+
+
+def hello_task():
+  print("Hello, Airflow!")
 
 dag = DAG(dag_id="hello", start_date=datetime(2022, 1, 1), schedule="0 0 * * *")
 
-@task(dag=dag)
-def hello_task():
-  print("Hello, Airflow!")
-  
-  
-hello = hello_task()
+hello = PythonOperator(task_id="hello", python_callable=hello_task, dag=dag)
 
 bash_task_1 = BashOperator(task_id="bash_task_1", bash_command="echo 'Hello from Bash 1!'", dag=dag)
 bash_task_2 = BashOperator(task_id="bash_task_2", bash_command="echo 'Hello from Bash 2!'", dag=dag)
